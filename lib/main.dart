e3fe276cd2765c5ccbe8082f46458100a98be142
Saikat Maity <saikat.maity@iecsl.co.in>
2022-03-17 20:58:53 +0530

    Chapter 4 Column completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,42 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        color: Colors.black,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.green,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.center,
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
             ),
-            Container(
-              padding: EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-            ),
-            Container(
-              padding: EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.orange,
-            ),
-          ],
+          ),
         ),
       ),
     );

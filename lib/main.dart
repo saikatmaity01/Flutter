db12933cda8a158d9da7854083b7478bf5800073
Saikat Maity <saikat.maity@iecsl.co.in>
2022-03-17 15:35:55 +0530

    Chapter 2 Container & Text completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(MaterialApp(
-    title: "Awesome App",
-    home: HomePage(),
-  ),);
+void main() {
+  runApp(
+    MaterialApp(
+      title: "Awesome App",
+      home: HomePage(),
+    ),
+  );
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -16,10 +17,37 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Center(child: Text("Hi Flutter")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 5,
+                offset: Offset(2.0, 5.0),
+              ),
+            ],
+            color: Colors.teal,
+            gradient: LinearGradient(
+              colors: [Colors.yellow, Colors.pink],
+            ),
+          ),
+          child: Text(
+            "I am flutter",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+            ),
+          ),
+        ),
       ),
     );
   }
-  
 }
